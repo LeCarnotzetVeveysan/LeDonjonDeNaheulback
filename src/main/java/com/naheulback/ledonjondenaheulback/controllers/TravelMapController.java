1fa7a6c9a67ff1337370d3c77af199052b20ca8d
Ghislain <ghislain.demael@gmail.com>
2022-11-25 11:55:50 +0100

    Setup dungeon information display on travel map

--- src/main/java/com/naheulback/ledonjondenaheulback/controllers/TravelMapController.java
+++ src/main/java/com/naheulback/ledonjondenaheulback/controllers/TravelMapController.java
@@ -2,23 +2,47 @@ package com.naheulback.ledonjondenaheulback.controllers;
 
 import com.naheulback.ledonjondenaheulback.Game;
 import com.naheulback.ledonjondenaheulback.LoadScene;
-import com.naheulback.ledonjondenaheulback.classes.Hero;
-import com.naheulback.ledonjondenaheulback.classes.PlayerData;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static javafx.scene.layout.BackgroundPosition.CENTER;
+import static javafx.scene.layout.BackgroundRepeat.NO_REPEAT;
+import static javafx.scene.layout.BackgroundRepeat.REPEAT;
+import static javafx.scene.layout.BackgroundSize.DEFAULT;
 
 public class TravelMapController {
 
+    private static String resPath = "src/main/resources/com/naheulback/ledonjondenaheulback/";
     @FXML
-    private ImageView donj4InfosIV;
+    private StackPane donj4InfoSP;
+    @FXML
+    private Label donj4StatsLbl;
 
-    public void initialize() throws FileNotFoundException {
+    public void initialize() throws IOException {
 
-        donj4InfosIV.setVisible(false);
+        donj4InfoSP.setVisible(false);
+        String path = resPath + "mapFiles/donj4_stats";
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String toDisplay = "";
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
+            toDisplay += line;
+            if(!line.equals("")){
+                toDisplay += "\n";
+            }
+        }
+        donj4StatsLbl.setText(toDisplay);
 
     }
     public void onDonj1ButtonClicked() throws IOException {
@@ -38,12 +62,8 @@ public class TravelMapController {
     }
 
     public void onDonj4ButtonHover() {
-
-        donj4InfosIV.setVisible(true);
+        donj4InfoSP.setVisible(true);
     }
 
-    public void onDonj4ButtonStoppedHover() {
-
-        donj4InfosIV.setVisible(false);
-    }
+    public void onDonj4ButtonStoppedHover() { donj4InfoSP.setVisible(false); }
 }
